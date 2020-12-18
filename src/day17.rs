f36b568746e0cdea47f1ba5fb1af4ae5e821c9a0
Philipp Bandow <bandow@formulastudent.de>
2020-12-18 04:06:23 +0100

    Refactor...

--- src/day17.rs
+++ src/day17.rs
@@ -3,6 +3,8 @@ use itertools::Itertools;
 use std::collections::HashSet;
 use std::mem::swap;
 
+const CYCLES: i32 = 6;
+
 #[aoc_generator(day17)]
 pub fn day17_generator(input: &str) -> HashSet<(i32, i32, i32, i32)> {
     let mut set: HashSet<(i32, i32, i32, i32)> = HashSet::new();
@@ -23,68 +25,46 @@ pub fn day17_generator(input: &str) -> HashSet<(i32, i32, i32, i32)> {
 
 #[aoc(day17, part1)]
 pub fn day17_part1(input: &HashSet<(i32, i32, i32, i32)>) -> usize {
-    let cycles = 6;
-    let mut input1 = input.clone();
-    let mut input2 = input.clone();
+    let mut cur = input.clone();
+    let mut pre = input.clone();
 
-    for i in 1..=cycles {
+    for i in 1..=CYCLES {
+        std::mem::swap(&mut pre, &mut cur);
         for (x, y, z) in iproduct!(-(i + 3)..=(i + 3), -(i + 3)..=(i + 3), -i..=i) {
             let adjacent = iproduct!(x - 1..=x + 1, y - 1..=y + 1, z - 1..=z + 1)
                 .into_iter()
-                .filter(|(i, j, k)| input2.contains(&(*i, *j, *k, 0)))
+                .filter(|(i, j, k)| pre.contains(&(*i, *j, *k, 0)))
                 .count();
-            match (input2.contains(&(x, y, z, 0)), adjacent) {
-                (true, 3..=4) => true,
-                (true, _) => input1.remove(&(x, y, z, 0)),
-                (false, 3) => input1.insert((x, y, z, 0)),
-                _ => true
-
+            match (pre.contains(&(x, y, z, 0)), adjacent) {
+                (true, 3..=4) => cur.insert((x, y, z, 0)),
+                (false, 3) => cur.insert((x, y, z, 0)),
+                _ => cur.remove(&(x, y, z, 0))
             };
         }
-        swap(&mut input1, &mut input2);
     }
-
-    iproduct!(
-        -(cycles + 4)..=(cycles + 4),
-        -(cycles + 4)..=(cycles + 4),
-        -cycles..=cycles
-    )
-    .into_iter()
-    .filter(|(x, y, z)| input2.contains(&(*x, *y, *z, 0)))
-    .count()
+    cur.len()
 }
 
 #[aoc(day17, part2)]
 pub fn day17_part2(input: &HashSet<(i32, i32, i32, i32)>) -> usize {
-    let cycles = 6;
-    let mut input1 = input.clone();
-    let mut input2 = input.clone();
+    let mut cur = input.clone();
+    let mut pre = input.clone();
 
-    for i in 1..=cycles {
+    for i in 1..=CYCLES {
+        std::mem::swap(&mut pre, &mut cur);
         for (x, y, z, w) in iproduct!(-(i + 3)..=(i + 3), -(i + 3)..=(i + 3), -i..=i, -i..=i) {
             let adjacent = iproduct!(x - 1..=x + 1, y - 1..=y + 1, z - 1..=z + 1, w - 1..=w + 1)
                 .into_iter()
-                .filter(|(i, j, k, l)| input2.contains(&(*i, *j, *k, *l)))
+                .filter(|(i, j, k, l)| pre.contains(&(*i, *j, *k, *l)))
                 .count();
-            match (input2.contains(&(x, y, z, w)), adjacent) {
-                (true, 3..=4) => true,
-                (true, _) => input1.remove(&(x, y, z, 0)),
-                (false, 3) => input1.insert((x, y, z, 0)),
-                _ => true
+            match (pre.contains(&(x, y, z, w)), adjacent) {
+                (true, 3..=4) => cur.insert((x, y, z, w)),
+                (false, 3) => cur.insert((x, y, z, w)),
+                _ => cur.remove(&(x, y, z, w)),
             };
         }
-        swap(&mut input1, &mut input2);
     }
-
-    iproduct!(
-        -(cycles + 4)..=(cycles + 4),
-        -(cycles + 4)..=(cycles + 4),
-        -cycles..=cycles,
-        -cycles..=cycles
-    )
-    .into_iter()
-    .filter(|(x, y, z, w)| input2.contains(&(*x, *y, *z, *w)))
-    .count()
+    cur.len()
 }
 
 #[cfg(test)]
